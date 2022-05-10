8765d9026f83fe1b5fa277ab6c361eff2c76b486
Administrator <hasan.alhen.98@gmail.com>
2022-05-10 15:55:37 +0300

    hasan1

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,10 @@ use App\Http\Controllers\Api\User\AuthUserController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
+|llll
 */
 
 Route::group(['prefix' => 'user','namespace' => 'User'],function(){
-    Route::get('login',[AuthUserController::class,'login']);
+    Route::post('login',[AuthUserController::class,'login']);
     Route::get('logout',[AuthUserController::class,'logout']) -> middleware(['auth.guard:user-api']);
 });
